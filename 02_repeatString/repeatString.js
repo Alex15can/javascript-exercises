86c57d9fcca91a07e787f4ece90945c1995e51cc
Alex <alexcannon_17@hotmail.com>
2022-04-19 18:12:51 -0400

    Exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR"
+    }
+    answer = ''
+    for (let i = 0; i< times; i++) {
+        answer = answer + word;
+    }
+    return answer
 };
 
 // Do not edit below this line

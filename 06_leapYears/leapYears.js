6cbe676827d50487cf84423f1ff9cd07a2f20728
Alex <alexcannon_17@hotmail.com>
2022-04-19 22:57:50 -0400

    ex6 with descending if statments.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yearToTest) {
+    if (yearToTest % 400 == 0) {
+        return true;
+    } else if (yearToTest% 100 == 0) {
+        return false;
+    } else if (yearToTest% 4 == 0) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line

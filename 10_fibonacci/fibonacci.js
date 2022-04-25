7ffe8a96a7461f188808fe1ab0102d0a3e8cff4d
Alex <alexcannon_17@hotmail.com>
2022-04-25 15:31:14 -0400

    10 fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(nTerm) {
+    if (nTerm < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < nTerm; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b
 };
 
 // Do not edit below this line

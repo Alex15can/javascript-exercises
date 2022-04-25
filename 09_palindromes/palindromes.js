0c4d94c8f48011dc2581ff1bffa8383ebf4af33a
Alex <alexcannon_17@hotmail.com>
2022-04-25 15:07:19 -0400

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text1) {
+    let text2 = text1.toLowerCase();
+    let text3 = text2.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]|_/g, "")
+    console.log(text3, text3.split("").reverse().join(""));
+    if (text3 == text3.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

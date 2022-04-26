3b5288d78c98330c413d86004b5757c7cceceb9d
Alex <alexcannon_17@hotmail.com>
2022-04-25 23:05:22 -0400

    finished 12 excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayObj) {
+    let resultAge = arrayObj.map(a => getAge(a.yearOfBirth,a.yearOfDeath));
+    let maxAge = Math.max(...resultAge);
+    let oldestIndex = resultAge.indexOf(maxAge);
+    return arrayObj[oldestIndex]
 };
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+      }
+    return death - birth;
+  };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
